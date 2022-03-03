56622e676abff10c664fed535dfa38a479675a2d
mmeosy1 <mehmetfidan94@hotmail.de>
2022-03-03 14:40:25 +0100

    init test

---
+++ agecalculation.py
@@ -0,0 +1,18 @@
+
+"""
+my fancy age calculation
+
+"""
+
+def calculate_age(birthdate, today):
+    """
+        my fancy age calculation
+
+    """
+    
+    year_difference = today.year - birthdate.year
+    no_birthday_this_year = (today.month, today.day) < (birthdate.month, birthdate.day)
+    
+    if no_birthday_this_year :
+        return year_difference -1
+    return year_difference   
---
+++ main.py
@@ -0,0 +1,18 @@
+
+import unittest
+from agecalculation import calculate_age
+
+class testagecalculation(unittest.TestCase)
+
+    def test_calculated_age_day_before_today(self)
+        birthday = datetime.datetime.strptime("22.05.2000", "%d.%m.%Y")
+        result = calculate_age(birthday)
+        self.assertEqual(result, 22)
+
+
+
+
+    def test_calculated_age_day_after_today(self)
+        birthday = datetime.datetime.strptime("22.05.2000", "%d.%m.%Y")
+        result = calculate_age(birthday)
+        self.assertEqual(result, 21)
